734a5b3344d9241d0a39fb254cceac52f73f2d64
AllyTiago <allytiagomail@gmail.com>
2017-11-03 13:51:12 -0300

    Create assisten.sql

---
+++ assisten.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `assisten` (
+  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `value` varchar(6) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
